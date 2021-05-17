c298181a303888bc3a44a04bedc718ecc1a8637c
S4YN3X <S4YN3X@gmail.com>
2021-05-17 21:29:03 +0300

    Полный recode проекта!

--- main/admin.py
+++ main/admin.py
@@ -4,5 +4,5 @@ from main.models.note import Note
 
 @admin.register(Note)
 class NoteAdmin(admin.ModelAdmin):
-    list_display = ['name', 'author']
-    list_display_links = ['name', 'author']
+    list_display = ['id', 'name', 'author']
+    list_display_links = ['id', 'name', 'author']
--- main/models/note.py
+++ main/models/note.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.contrib.auth.models import User
 from django.db import models
 from django.conf import settings
@@ -7,6 +9,7 @@ class Note(models.Model):
     name = models.CharField(max_length=255, verbose_name='Название заметки')
     text = models.TextField(verbose_name='Текст заметки')
     author = models.ForeignKey(User, verbose_name='Автор', on_delete=models.RESTRICT)
+    #id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
     def __str__(self):
         return self.name
